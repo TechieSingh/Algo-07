a2ce9ee306ffe8e82f357c63a4d097cf7a29cc81
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2024-04-15 14:31:51 -0400

    Time: 267 ms (52.52%) | Memory: 20.6 MB (38.86%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,11 +1,9 @@
 class Solution(object):
     def findDuplicates(self, nums):
         ans = []
+        dup = set()
         for num in nums:
-            index = abs(num) - 1  # Get the index corresponding to the number
-            if nums[index] < 0:
-                ans.append(abs(num))  # If the number at this index is negative, it means it has appeared before
-            else:
-                nums[index] = -nums[index]  # Mark the number as visited by using logical NOT gate (negation)
-
+            if num in dup:
+                ans.append(num)
+            dup.add(num)
         return ans
