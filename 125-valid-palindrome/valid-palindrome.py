b3029b12d5de66f027157514913507549ad4ac55
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2024-05-14 20:08:34 -0400

    Time: 31 ms (98.81%) | Memory: 18.1 MB (18.63%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,12 +3,7 @@ class Solution:
         n=len(s)
         ls = re.sub(r'[^a-zA-Z0-9]', '', s)
         ls=ls.lower()
-        print(ls) 
-        p=len(ls)       
-        for k in range(p):
-            if ls[k]!=ls[p-1-k]:
-                return False
-        return True
+        return ls==ls[::-1]
 
 
 
