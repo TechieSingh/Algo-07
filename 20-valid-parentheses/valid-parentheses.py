6e904a1bc10a3c7521b9286ccab41e4c384ac568
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2024-05-23 19:30:27 -0400

    Time: 34 ms (67.26%) | Memory: 16.7 MB (5.89%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in bracket_map.values():
+                stack.append(char)
+            elif char in bracket_map:
+                if stack and stack[-1] == bracket_map[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                return False
+        return not stack
