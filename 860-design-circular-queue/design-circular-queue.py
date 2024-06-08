63fb3e639b31fb5a87e013aa49baf9392c2ea058
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2024-06-08 11:31:43 -0400

    Time: 60 ms (50.45%) | Memory: 17.3 MB (31.53%) - LeetSync

--- 860-design-circular-queue/design-circular-queue.py
+++ 860-design-circular-queue/design-circular-queue.py
@@ -1,52 +1,62 @@
 class ListNode:
-    def __init__(self, value=0, next=None):
-        self.value = value
+    def __init__(self, val=0, next=None):
+        self.val = val
         self.next = next
+class Slist:
+    def __init__(self):
+        self._first = None
+        self._last = None
+        self._len = 0
 
 class MyCircularQueue:
     def __init__(self, k: int):
-        self.head = None
-        self.tail = None
-        self.max_size = k
-        self.size = 0
+        self._capacity = k
+        self._size = 0
+        self._head = None
+        self._tail = None
 
     def enQueue(self, value: int) -> bool:
-        if self.isFull():
+        if self._size == self._capacity:
             return False
         new_node = ListNode(value)
-        if self.isEmpty():
-            self.head = self.tail = new_node
-            self.tail.next = self.head
+        if self._size == 0:
+            self._head = self._tail = new_node
+            self._tail.next = self._head
         else:
-            self.tail.next = new_node
-            self.tail = new_node
-            self.tail.next = self.head
-        self.size += 1
+            self._tail.next = new_node
+            self._tail = new_node
+            self._tail.next = self._head
+        self._size += 1
         return True
 
     def deQueue(self) -> bool:
-        if self.isEmpty():
+        if self._size == 0:
             return False
-        if self.head == self.tail:
-            self.head = self.tail = None
+        if self._size == 1:
+            self._head = self._tail = None
         else:
-            self.head = self.head.next
-            self.tail.next = self.head
-        self.size -= 1
+            self._head = self._head.next
+            self._tail.next = self._head
+        self._size -= 1
         return True
 
     def Front(self) -> int:
-        if self.isEmpty():
+        if self._size == 0:
             return -1
-        return self.head.value
+        return self._head.val
 
     def Rear(self) -> int:
-        if self.isEmpty():
+        if self._size == 0:
             return -1
-        return self.tail.value
+        return self._tail.val
 
     def isEmpty(self) -> bool:
-        return self.size == 0
+        return self._size == 0
 
     def isFull(self) -> bool:
-        return self.size == self.max_size
+        return self._size == self._capacity
+
+
+
+
+
