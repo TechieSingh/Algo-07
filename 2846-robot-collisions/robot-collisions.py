ffb3e05ea4211f340a6e9dcc6c3b4c5748674847
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2024-07-13 12:56:05 -0400

    Time: 933 ms (86.54%) | Memory: 34.8 MB (76.92%) - LeetSync

--- 2846-robot-collisions/robot-collisions.py
+++ 2846-robot-collisions/robot-collisions.py
@@ -1,40 +1,28 @@
 from typing import List
 
 class Solution:
-    class Robot:
-        def __init__(self, position: int, health: int, direction: str, index: int):
-            self.position = position
-            self.health = health
-            self.direction = direction
-            self.index = index
-
     def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
         n = len(positions)
-        vals = [self.Robot(positions[i], healths[i], directions[i], i) for i in range(n)]
-        vals.sort(key=lambda x: x.position)
-
-        stack = []
-        for robot in vals:
-            if robot.direction == 'R':
-                stack.append(robot)
-                continue
-
-            gone = False
-            while stack and stack[-1].health <= robot.health and stack[-1].direction == 'R':
-                if stack[-1].health == robot.health:
-                    stack.pop()
-                    gone = True
-                    break
-                robot.health -= 1
-                stack.pop()
-
-            if not gone and stack and stack[-1].direction == 'R' and stack[-1].health > robot.health:
-                stack[-1].health -= 1
-                gone = True
-
-            if not gone:
-                stack.append(robot)
-
-        stack.sort(key=lambda x: x.index)
-
-        return [robot.health for robot in stack]
+        ind = [i for i in range(n)]
+        ind.sort(key=lambda x: positions[x])
+        s = []
+        for x in ind:
+            if directions[x] == 'L':
+                while s:
+                    y = s[-1]
+                    if healths[x] == healths[y]:
+                        healths[x] = healths[y] = 0
+                        s.pop()
+                        break
+                    if healths[x] > healths[y]:
+                        healths[x] -= 1
+                        healths[y] = 0
+                        s.pop()
+                    else:
+                        healths[x] = 0
+                        healths[y] -= 1
+                        break
+            else:
+                s.append(x)
+        r = [x for x in healths if x]
+        return r
