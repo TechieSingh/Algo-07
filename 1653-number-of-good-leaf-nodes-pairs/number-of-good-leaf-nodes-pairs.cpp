e817ed877fddf3e27aa069544caab5e666242bbd
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2024-07-18 17:54:47 -0400

    Time: 44 ms (71.63%) | Memory: 33.1 MB (74.80%) - LeetSync

--- 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.cpp
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.cpp
@@ -10,39 +10,41 @@
  * };
  */
 class Solution {
-public:
-    int countPairs(TreeNode* root, int distance) {
-        unordered_map<TreeNode*, vector<TreeNode*>> map;
-        vector<TreeNode*> leaves;
-        findLeaves(root, {}, leaves, map);
-        int res = 0;
-        for (int i = 0; i < leaves.size(); i++) {
-            for (int j = i + 1; j < leaves.size(); j++) {
-                vector<TreeNode*>& list_i = map[leaves[i]];
-                vector<TreeNode*>& list_j = map[leaves[j]];
-                for (int k = 0; k < min(list_i.size(), list_j.size()); k++) {
-                    if (list_i[k] != list_j[k]) {
-                        int dist = list_i.size() - k + list_j.size() - k;
-                        if (dist <= distance) res++;
-                        break;
-                    }
-                }
+    int answer = 0;
+    vector<int> dfs(TreeNode* node, int distance) {
+        if(node == nullptr) {
+            return {};
+        }
+        if(node->left == nullptr && node->right == nullptr) {
+            return {1};
+        }
+        vector<int> left = dfs(node->left, distance);
+        vector<int> right = dfs(node->right, distance);
+        
+        for(auto l : left) {
+            for(auto r : right) {
+                if(l + r <= distance) {
+                    answer++;
+                } 
+            }
+        }
+
+        vector<int> res;
+        for(auto l : left) {
+            if(l + 1 < distance) {
+                res.push_back(l + 1);
+            }
+        }
+        for(auto r : right) {
+            if(r + 1 < distance) {
+                res.push_back(r + 1);
             }
         }
         return res;
     }
-
-private:
-    void findLeaves(TreeNode* node, vector<TreeNode*> trail, vector<TreeNode*>& leaves, unordered_map<TreeNode*, vector<TreeNode*>>& map) {
-        if (!node) return;
-        vector<TreeNode*> tmp(trail);
-        tmp.push_back(node);
-        if (!node->left && !node->right) {
-            map[node] = tmp;
-            leaves.push_back(node);
-            return;
-        }
-        findLeaves(node->left, tmp, leaves, map);
-        findLeaves(node->right, tmp, leaves, map);
+public:
+    int countPairs(TreeNode* root, int distance) {
+        dfs(root, distance);
+        return answer;
     }
 };
