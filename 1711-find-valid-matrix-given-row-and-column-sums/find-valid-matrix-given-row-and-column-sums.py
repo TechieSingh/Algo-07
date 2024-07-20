ad92dffd70a025a4cf54ff83d1270278a488eb2e
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2024-07-20 11:02:56 -0400

    Time: 360 ms (95.67%) | Memory: 22.2 MB (7.22%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
@@ -1,12 +1,34 @@
 class Solution:
     def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
-        res = [[0] * len(colSum) for _ in range(len(rowSum))]
-        for i in range(len(rowSum)):
-            for j in range(len(colSum)):
-                val = min(rowSum[i], colSum[j])
-                res[i][j] = val
-                rowSum[i] -= val
-                colSum[j] -= val
-        return res    
+        sol=2
+        if sol==1:
+            res = [[0] * len(colSum) for _ in range(len(rowSum))]
+            for i in range(len(rowSum)):
+                for j in range(len(colSum)):
+                    val = min(rowSum[i], colSum[j])
+                    res[i][j] = val
+                    rowSum[i] -= val
+                    colSum[j] -= val
+            return res 
 
-       
+
+        if sol==2:
+            col_sum = colSum
+            row_sum = rowSum
+            
+            mat = [[0]*len(col_sum) for i in range(len(row_sum))]
+            i = 0
+            j = 0
+            while i < len(row_sum) and j < len(col_sum):
+                mat[i][j] = min(row_sum[i], col_sum[j])
+                if row_sum[i] == col_sum[j]:
+                    i += 1
+                    j += 1
+                elif row_sum[i] > col_sum[j]:
+                    row_sum[i] -= col_sum[j]
+                    j += 1
+                else:
+                    col_sum[j] -= row_sum[i]
+                    i += 1
+
+            return mat
