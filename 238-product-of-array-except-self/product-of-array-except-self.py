9d44ed4011c13b7db7acc67af56e32e446b59317
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2024-07-24 07:58:52 -0400

    Time: 251 ms (97.01%) | Memory: 25.7 MB (47.20%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,69 +1,18 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        work = [] 
-        ans = []  
-        if False:
-            b = self.L0238(nums, ans, work, "Brute Force")
-        if False:
-            b = self.L0238(nums, ans, work, "Use Division")
-        if False:
-            b = self.L0238(nums, ans, work, "n time n space")
-        if True:
-            b = self.L0238(nums, ans, work, "n time 1 space")
+        i=0
+        n=len(nums)
+        res=[0]*n
+        left=1
+        for j in range(n):
+            res[j]=left
+            left=left*nums[j]
         
-        return ans
-
-    def L0238(self, nums, ans, work, method):
-        print(f"Processing with method: {method}")
-        if method == "Brute Force":
-            for i in range(len(nums)):
-                product = 1
-                for j in range(len(nums)):
-                    if i != j:
-                        product *= nums[j]
-                ans.append(product)
-        
-        elif method == "Use Division":
-            total_product = 1
-            zero_count = nums.count(0) 
-            if zero_count == 0:
-                for num in nums:
-                    total_product *= num
-                for num in nums:
-                    ans.append(total_product // num)
-            elif zero_count == 1:
-                for num in nums:
-                    if num != 0:
-                        total_product *= num
-                for num in nums:
-                    if num == 0:
-                        ans.append(total_product)
-                    else:
-                        ans.append(0)
-            else:
-                ans.extend([0] * len(nums))
-
+        right=1
+        for k in range(n-1,-1,-1):
+            res[k]=res[k]*right
+            right=right*nums[k]
+            
+        return res
 
         
-        elif method == "n time n space":
-            left = [1] * len(nums)
-            right = [1] * len(nums)
-            for i in range(1, len(nums)):
-                left[i] = left[i-1] * nums[i-1]
-            for i in range(len(nums) - 2, -1, -1):
-                right[i] = right[i+1] * nums[i+1]
-            for i in range(len(nums)):
-                ans.append(left[i] * right[i])
-        
-        elif method == "n time 1 space":
-            ans.extend([1] * len(nums))
-            left = 1
-            for i in range(len(nums)):
-                ans[i] = left
-                left *= nums[i]
-            right = 1
-            for i in range(len(nums) - 1, -1, -1):
-                ans[i] *= right
-                right *= nums[i]
-
-        return ans
