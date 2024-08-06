c80ce75182829640e6690733446eec14aec6e79a
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2024-08-06 10:24:52 -0400

    Time: 96 ms (99.28%) | Memory: 17.6 MB (73.03%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -1,15 +1,11 @@
 class Solution:
-    def minimumPushes(self, input_text: str) -> int:
-        letter_counts = [0] * 26
-        for char in input_text:
-            letter_counts[ord(char) - ord('a')] += 1
-            
-        sorted_counts = sorted(letter_counts, reverse=True)
-        
-        total_key_presses = 0
-        for index, count in enumerate(sorted_counts):
-            if count == 0:
-                break
-            total_key_presses += (index // 8 + 1) * count
-        
-        return total_key_presses
+    def minimumPushes(self, word: str) -> int:
+        l=[0]*(32)
+        for i in range(26):
+            l[i]=word.count(chr(97+i))
+        l.sort(reverse=True)
+        res=0
+        for i in range(4):
+            for j in range(8):
+                res+=(i+1)*l[8*i+j]
+        return res
