801989f0a43dc5c19c09ad3026e2ea8a635c5507
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2024-08-07 18:11:47 -0400

    Time: 32 ms (83.16%) | Memory: 16.7 MB (47.65%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.py
+++ 273-integer-to-english-words/integer-to-english-words.py
@@ -1,32 +1,30 @@
 class Solution:
     def numberToWords(self, num: int) -> str:
-        if num == 0:
-            return "Zero"
-        ones = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
-        tens = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
-        teens = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
-        suffixes = ["", "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion", "Sextillion", "Septillion", "Octillion", "Nonillion", "Decillion"]
-        words = []
-        i = 0
-        while num > 0:
-            triplet = num % 1000
-            num = num // 1000
-            if triplet == 0:
-                i += 1
-                continue
-            temp = []
-            if triplet // 100 > 0:
-                temp.append(ones[triplet // 100])
-                temp.append("Hundred")
-            if triplet % 100 >= 10 and triplet % 100 <= 19:
-                temp.append(teens[triplet % 10])
+        def helper(num):
+        
+            if num < 20:
+                s = belowTwenty[num]
+            elif num < 100:
+                s = tens[num // 10] + ' ' + belowTwenty[num % 10]
+            elif num < 1000:
+                s = helper(num // 100) + ' Hundred ' + helper(num % 100)
+            elif num < 1000000:
+                s = helper(num // 1000) + ' Thousand ' + helper(num % 1000)
+            elif num < 1000000000:
+                s = helper(num // 1000000) + ' Million ' + helper(num % 1000000)
             else:
-                if triplet % 100 >= 20:
-                    temp.append(tens[triplet % 100 // 10])
-                if triplet % 10 > 0:
-                    temp.append(ones[triplet % 10])
-            if i > 0:
-                temp.append(suffixes[i])
-            words = temp + words
-            i += 1
-        return " ".join(words)
+                s = helper(num // 1000000000) + ' Billion ' + helper(num % 1000000000)
+
+            return s.strip()
+
+        if num == 0:
+            return 'Zero'
+
+        belowTwenty = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight',
+                       'Nine', 'Ten', 'Eleven', 'Twelve','Thirteen', 'Fourteen', 'Fifteen',
+                       'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
+
+        tens = ['', 'Ten', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 
+                'Seventy', 'Eighty', 'Ninety']
+
+        return helper(num)
