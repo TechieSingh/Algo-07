36177737b6fd4c700542b4913d09f27daf80af3c
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2024-08-28 10:45:44 -0400

    Time: 92 ms (58.44%) | Memory: 16.5 MB (85.28%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,14 +1,23 @@
+# (1) Use separate for loops to go through the rows, columns, and each 3x3 grid. Create a new set for each one to check for any duplicate values => O(9^2) = O(1) time complexity because we have a fixed 9x9 sudoku board. 
+# (2) Use one set of for loops and a unique HashSet for the rows, columns, and 3x3 grids. Check for any duplicates => O(9^2) = O(1) time complexity because we have a fixed 9x9 sudoku board.
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        seen = set()
-        for k in range(81):
-            i, j = divmod(k, 9)
-            c = board[i][j]
-            if c != '.':
-                if (c, i) in seen or (j, c) in seen or (i // 3, j // 3, c) in seen:
-                    return False
-                seen.add((c, i))
-                seen.add((j, c))
-                seen.add((i // 3, j // 3, c))
-        return True
+        rows = defaultdict(set) # row : [values]
+        cols = defaultdict(set) # col : [values]
+        grids = defaultdict(set) # (row // 3, col // 3) : [values]
         
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] != ".":
+                    if (board[r][c] in rows[r] or 
+                        board[r][c] in cols[c] or 
+                        board[r][c] in grids[(r // 3, c // 3)]):
+                        return False
+                    
+                    rows[r].add(board[r][c])
+                    cols[c].add(board[r][c])
+                    # store element in the correct grid using a tuple key
+                    grids[(r // 3, c // 3)].add(board[r][c])
+                    
+        return True
