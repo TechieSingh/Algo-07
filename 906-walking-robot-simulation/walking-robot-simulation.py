668d4ca1276e2a953f42516287792e6bc7a6630e
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2024-09-04 10:53:42 -0400

    Time: 267 ms (96.52%) | Memory: 23.3 MB (45.08%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.py
+++ 906-walking-robot-simulation/walking-robot-simulation.py
@@ -1,24 +1,25 @@
 class Solution:
     def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
 
-        current = [0, 0]
-        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]  
-        direction_index = 0  
-        result = 0
-
         obstacle_set = set(map(tuple, obstacles))
 
-        for command in commands:
-            if command == -1:  
-                direction_index = (direction_index + 1) % 4
-            elif command == -2:  
-                direction_index = (direction_index - 1) % 4
-            else:
-                for _ in range(command):
-                    # Calculate next position
-                    next_pos = [current[0] + directions[direction_index][0], current[1] + directions[direction_index][1]]
-                    if tuple(next_pos) not in obstacle_set:  # Check if next position is an obstacle
-                        current = next_pos  # Update current position
-                        result = max(result, current[0] ** 2 + current[1] ** 2)  # Update result with max distance squared
+        x, y = 0, 0
+        dx, dy = 0, 1
 
-        return result
+        max_dist = 0
+
+        for command in commands:
+            if command == -2: # turn left 90 deg
+                dx, dy = -dy, dx
+            elif command == -1: # turn right 90 deg
+                dx, dy = dy, -dx
+            else:
+                move = 0
+                while move < command and (x + dx, y + dy) not in obstacle_set:
+                    x += dx
+                    y += dy
+                    move += 1
+                max_dist = max(x**2 + y**2, max_dist)
+                
+        
+        return max_dist
