23658798b7ca0654f22d36567204cae375640215
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2024-09-05 14:07:25 -0400

    Time: 974 ms (69.25%) | Memory: 27.4 MB (27.01%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.py
+++ 2155-find-missing-observations/find-missing-observations.py
@@ -1,15 +1,10 @@
 class Solution:
     def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
         m=len(rolls)
-        totalsum=mean*(n+m)
-        missingsum=totalsum-sum(rolls)
-        if missingsum>6*n or missingsum<n:
-            return []
-        res=[]
-        while n:
-            curr=min(6,missingsum-n+1)
-            res.append(curr)
-            missingsum-=curr
-            n-=1
-        return res
+        sum_rolles=sum(rolls)
+        total=(n+m)*mean
+        miss=total-sum_rolles
+        if miss>6*n or miss<n: return []
+        q,r=divmod(miss, n)
+        return [q+1]*r+[q]*(n-r)
         
