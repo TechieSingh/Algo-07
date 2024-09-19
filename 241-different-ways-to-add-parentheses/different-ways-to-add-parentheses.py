279afe3eb4f2ba5788430744f5e4cc103ef99a03
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2024-09-18 23:30:10 -0400

    Time: 35 ms (75.18%) | Memory: 16.6 MB (90.68%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -1,19 +1,26 @@
-class Solution(object):
-    def diffWaysToCompute(self, input):
-        m = {}
-        return self.dfs(input, m)
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        results=[]
+        if len(expression)==0:
+            return results
+        if len(expression)==1:
+            return [int(expression)]
+        if len(expression)==2 and expression[0].isdigit():
+            return [int(expression)]
+        for i,current_char in enumerate(expression):
+            if current_char.isdigit():
+                continue
+            left_results=self.diffWaysToCompute(expression[:i])
+            right_results=self.diffWaysToCompute(expression[i+1:])
+            for left_val in left_results:
+                for right_val in right_results:
+                    if current_char == "+":
+                        results.append(left_val + right_val)
+                    elif current_char == "-":
+                        results.append(left_val - right_val)
+                    elif current_char == "*":
+                        results.append(left_val * right_val)
+
+        return results
+
         
-    def dfs(self, input, m):
-        if input in m:
-            return m[input]
-        if input.isdigit():
-            m[input] = int(input)
-            return [int(input)]
-        ret = []
-        for i, c in enumerate(input):
-            if c in "+-*":
-                l = self.diffWaysToCompute(input[:i])
-                r = self.diffWaysToCompute(input[i+1:])
-                ret.extend(eval(str(x)+c+str(y)) for x in l for y in r)
-        m[input] = ret
-        return ret
